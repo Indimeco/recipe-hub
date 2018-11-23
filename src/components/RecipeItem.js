76b26510631d5d1c3643beedcfa47a2d7efaaa9e
Indimeco <18373729+Indimeco@users.noreply.github.com>
2018-11-23 17:36:55 +1100

    Added template for recipe control buttons

--- src/components/RecipeItem.js
+++ src/components/RecipeItem.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import RecipeItemControls from './RecipeItemControls';
 
 export default class Nav extends React.Component {
   render() {
@@ -6,6 +7,7 @@ export default class Nav extends React.Component {
       <a key={this.props.name} href={this.props.link}>
         <h2>{this.props.name}</h2>
         <img src={this.props.preview} />
+        <RecipeItemControls />
       </a>
     );
   }
---
+++ src/components/RecipeItemControls.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class Nav extends React.Component {
+  render() {
+    return (
+      <div>
+        <button>Edit</button>
+        <button>Favorite</button>
+        <button>View</button>
+      </div>
+    );
+  }
+}

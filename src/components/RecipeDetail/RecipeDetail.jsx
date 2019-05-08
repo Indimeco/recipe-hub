ab4c2cc58ba22623a2d8de04e6e7113726668de9
Indimeco <18373729+Indimeco@users.noreply.github.com>
2019-05-08 18:17:46 +1000

    front end for cook time

--- src/components/RecipeDetail/RecipeDetail.jsx
+++ src/components/RecipeDetail/RecipeDetail.jsx
@@ -1,17 +1,39 @@
 import React from 'react';
 import styled from 'styled-components';
 import withColor from '../../hocs/withColor';
-import { recipeDetailStyle, recipeIntroStyle } from './RecipeDetail.style.js';
+import { recipeDetailStyle, recipeIntroStyle, timeBoxStyle, timeBoxItemStyle } from './RecipeDetail.style.js';
 import Heading from '../../components/Heading/Heading';
 import Image from '../../components/Image/Image';
 
-const CookTime = ({ active, ready, className, ...restProps }) => {
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+	faClock,
+	faRunning,
+	faHourglass,
+	faEquals,
+	faPlus
+} from '@fortawesome/free-solid-svg-icons';
+
+const TimeBox = styled.span`${timeBoxStyle}`;
+
+const CookTime = withColor(({ active, ready, className, color, ...restProps }) => {
+	const getTime = (time) => {
+		const hours = Math.round(time / 60);
+		const remainder = Math.round(time % 60);
+
+		return `${hours ? hours + 'hr' : ''}${hours && remainder ? ' ' : ''}${remainder ? remainder + 'm' : ''}`;
+	};
+
 	return (
-		<span className={className}>
-			{active} + {ready} = {active + ready}
-		</span>
+		<TimeBox className={className} color={color} {...restProps}>
+			<FontAwesomeIcon icon={faClock} />{getTime(active + ready)}
+			<FontAwesomeIcon icon={faEquals} />
+			<FontAwesomeIcon icon={faRunning} />{getTime(active)}
+			<FontAwesomeIcon icon={faPlus} />
+			<FontAwesomeIcon icon={faHourglass} />{getTime(ready)}
+		</TimeBox>
 	);
-};
+});
 
 const IngredientsList = ({ ingredients, className, ...restProps }) => {
 	return (
@@ -53,6 +75,7 @@ class RecipeDetail extends React.Component {
 				<RecipeIntro color={color}>
 					<Heading el="h2">{recipe.name}</Heading>
 					<CookTime
+						color="main"
 						active={recipe['active time']}
 						ready={recipe['ready time']}
 					/>
--- src/components/RecipeDetail/RecipeDetail.style.js
+++ src/components/RecipeDetail/RecipeDetail.style.js
@@ -1,6 +1,6 @@
 import { css } from 'styled-components';
 
-export const recipeIntroStyle = ({ color }) => css`
+export const recipeIntroStyle = ({ color, ...props }) => css`
   color: ${color.fg};
   background-color: ${color.bg};
   display: block;
@@ -8,11 +8,23 @@ export const recipeIntroStyle = ({ color }) => css`
   @media screen and (min-width: 768px) {
     display: grid;
     grid-template-columns: 1fr 2fr;
-    grid-column-gap: 10px;
+    grid-column-gap: ${props.theme.spacing.small};
   }
 `;
 
 export const recipeDetailStyle = ({ color }) => css`
   color: ${color.fg};
   background-color: ${color.bg};
+`;
+
+export const timeBoxStyle = ({ color, ...props }) => css`
+  color: ${color.fg};
+  background-color: ${color.bg};
+  padding: ${props.theme.spacing.small};
+  margin: ${props.theme.spacing.small};
+  text-align: center;
+
+  svg {
+    margin: 0 5px;
+  }
 `;

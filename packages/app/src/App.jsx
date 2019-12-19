46e98ed48ef65e40ee2eb795f0c428f9e5dc29fc
Indimeco <18373729+Indimeco@users.noreply.github.com>
2019-12-19 22:11:22 +1100

    refactor(app): integrate graphql wip

--- packages/app/src/App.jsx
+++ packages/app/src/App.jsx
@@ -1,98 +1,106 @@
-import React from 'react';
-import styled from 'styled-components';
-import PropTypes from 'prop-types';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import React, { useEffect } from "react";
+import styled from "styled-components";
+import PropTypes from "prop-types";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { useQuery } from "@apollo/react-hooks";
+import { gql } from "apollo-boost";
 
-import componentStyle from './App.style';
-import Banner from './components/Banner/Banner';
-import Navbar from './components/Navbar/Navbar';
-import Footer from './components/Footer/Footer';
-import RecipeArea from './components/RecipeArea/RecipeArea';
-import Form from './components/Form/Form';
-import RecipeDetail from './components/RecipeDetail/RecipeDetail';
-import ErrorPage from './components/ErrorPage/ErrorPage';
-import withColor from './hocs/withColor';
+import componentStyle from "./App.style";
+import Banner from "./components/Banner/Banner";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
+import RecipeArea from "./components/RecipeArea/RecipeArea";
+import Form from "./components/Form/Form";
+import RecipeDetail from "./components/RecipeDetail/RecipeDetail";
+import ErrorPage from "./components/ErrorPage/ErrorPage";
+import withColor from "./hocs/withColor";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { book: null };
-  }
-
-  async componentDidMount() {
-    const response = await fetch(`/api/books/${this.props.bookId}`);
-    const book = await response.json();
-    this.setState({ book });
-  }
-
-  updateRecipe() {
-    return async (id, key, payload) =>
-      await fetch(`/api/books/${this.props.bookId}/${id}?key=${key}`, {
-        method: 'PUT',
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(payload),
-      });
-  }
-
-  render() {
-    const { className } = this.props;
-    return (
-      <Router>
-        <div className={className}>
-          <Navbar
-            color="aux"
-            banner={
-              <Banner color="main" el="h1">
-                Recipe Hub
-              </Banner>
-            }
-          />
-          <div className="content">
-            <Switch>
-              <Route
-                path="/"
-                exact
-                render={() => (this.state.book ? <RecipeArea book={this.state.book} color="main" /> : 'Loading...')}
-              />
-
-              <Route path="/new" render={() => <Form color="main" />} />
-
-              <Route
-                path="/view/:id"
-                render={({ match }) =>
-                  this.state.book ? (
-                    <RecipeDetail
-                      match={match}
-                      book={this.state.book}
-                      handleSubmit={this.updateRecipe()}
-                      color="root"
-                    />
-                  ) : (
-                    'Loading...'
-                  )
-                }
-              />
-
-              <Route render={() => <ErrorPage />} />
-            </Switch>
-          </div>
-          <Footer color="aux" />
-        </div>
-      </Router>
-    );
+const queryBook = (bookId) => gql`
+{
+  book(bookId: ${bookId}) {
+    meta {
+      name
+    }
+    recipes {
+      id
+      name
+      ingredients {
+        name
+        quantity
+        unit
+      }
+      directions
+      waitingTime
+      activeTime
+      previewImage
+      recipeSource
+    }
   }
 }
+`;
+
+const App = ({ className, bookId }) => {
+  const { loading, error, book } = useQuery(queryBook(bookId));
+  const updateRecipe = () => null;
+  return (
+    <Router>
+      <div className={className}>
+        <Navbar
+          color="aux"
+          banner={
+            <Banner color="main" el="h1">
+              Recipe Hub
+            </Banner>
+          }
+        />
+        <div className="content">
+          <Switch>
+            <Route
+              path="/"
+              exact
+              render={() =>
+                book ? (
+                  <RecipeArea book={book} color="main" />
+                ) : (
+                    "Loading..."
+                  )
+              }
+            />
+
+            <Route path="/new" render={() => <Form color="main" />} />
+
+            <Route
+              path="/view/:id"
+              render={({ match }) =>
+                this.state.book ? (
+                  <RecipeDetail
+                    match={match}
+                    book={book}
+                    handleSubmit={this.updateRecipe()}
+                    color="root"
+                  />
+                ) : (
+                    "Loading..."
+                  )
+              }
+            />
+
+            <Route render={() => <ErrorPage />} />
+          </Switch>
+        </div>
+        <Footer color="aux" />
+      </div>
+    </Router>
+  );
+};
 
 App.propTypes = {
   bookId: PropTypes.string,
-  className: PropTypes.string,
+  className: PropTypes.string
 };
 
 export default withColor(
   styled(App)`
     ${componentStyle}
-  `,
+  `
 );
--- packages/app/src/index.js
+++ packages/app/src/index.js
@@ -3,20 +3,28 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { ThemeProvider } from 'styled-components';
 import { ModalProvider } from 'styled-react-modal';
-
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import theme from './styles/theme';
 import { ColorProvider } from './hocs/withColor';
 
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000',
+});
+
 ReactDOM.render(
-  <ThemeProvider theme={theme}>
-    <ColorProvider.Provider value="root">
-      <ModalProvider>
-        <App bookId="1" />
-      </ModalProvider>
-    </ColorProvider.Provider>
-  </ThemeProvider>,
+  <ApolloProvider client={client}>
+    <ThemeProvider theme={theme}>
+      <ColorProvider.Provider value="root">
+        <ModalProvider>
+          <App bookId="1" />
+        </ModalProvider>
+      </ColorProvider.Provider>
+    </ThemeProvider>
+  </ApolloProvider>,
   document.getElementById('root'),
 );
 

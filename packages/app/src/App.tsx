a850594ec39535838c06f80824da767050d8a98c
Indimeco <18373729+Indimeco@users.noreply.github.com>
2019-12-22 19:50:01 +1100

    feat(app): introduce context reducer pattern

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { ReactElement, useEffect } from 'react';
 import styled from 'styled-components';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { useQuery } from '@apollo/react-hooks';
@@ -13,8 +13,9 @@ import Form from './components/Form/Form';
 import RecipeDetail from './components/RecipeDetail/RecipeDetail';
 import ErrorPage from './components/ErrorPage/ErrorPage';
 import withColor from './hocs/withColor';
+import { useGlobalState } from './store';
 
-const queryBook = (bookId: string) => gql`
+const queryBook = (bookId: string): any => gql`
 {
   book(bookId: "${bookId}") {
     meta {
@@ -40,7 +41,15 @@ const queryBook = (bookId: string) => gql`
 
 const App = ({ className, bookId }: { className: string; bookId: string }): any => {
   const { loading, error, data } = useQuery(queryBook(bookId));
+  const { dispatch } = useGlobalState();
+
   const book = data?.book;
+  useEffect(() => {
+    if (book) {
+      // FIXME firing twice during render
+      dispatch({ payload: book, type: 'activeBook' });
+    }
+  }, [bookId, book, dispatch]);
   const updateRecipe = () => () => null;
   return (
     <Router>
@@ -55,22 +64,26 @@ const App = ({ className, bookId }: { className: string; bookId: string }): any 
         />
         <div className="content">
           <Switch>
-            <Route path="/" exact render={() => (book ? <RecipeArea book={book} color="main" /> : 'Loading...')} />
+            <Route
+              path="/"
+              exact
+              render={(): ReactElement => (book ? <RecipeArea book={book} color="main" /> : <div>Loading...</div>)}
+            />
 
             <Route path="/new" render={() => <Form color="main" />} />
 
             <Route
               path="/view/:id"
-              render={({ match }: { match: any }) =>
+              render={({ match }: { match: any }): ReactElement =>
                 book ? (
                   <RecipeDetail match={match} book={book} handleSubmit={updateRecipe()} color="root" />
                 ) : (
-                  'Loading...'
+                  <div>Loading...</div>
                 )
               }
             />
 
-            <Route render={() => <ErrorPage />} />
+            <Route render={(): ReactElement => <ErrorPage />} />
           </Switch>
         </div>
         <Footer color="aux" />
--- packages/app/src/index.js
+++ packages/app/src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import { ThemeProvider } from 'styled-components';
 import { ModalProvider } from 'styled-react-modal';
 import { ApolloProvider } from '@apollo/react-hooks';
 import ApolloClient from 'apollo-boost';
+
+import './index.css';
 import App from './App';
+import { StateProvider } from './store';
 import * as serviceWorker from './serviceWorker';
 import theme from './styles/theme';
 import { ColorProvider } from './hocs/withColor';
-
 
 const client = new ApolloClient({
   uri: 'http://localhost:4000',
@@ -18,11 +19,13 @@ const client = new ApolloClient({
 ReactDOM.render(
   <ApolloProvider client={client}>
     <ThemeProvider theme={theme}>
-      <ColorProvider.Provider value="root">
-        <ModalProvider>
-          <App bookId="1" />
-        </ModalProvider>
-      </ColorProvider.Provider>
+      <StateProvider>
+        <ColorProvider.Provider value="root">
+          <ModalProvider>
+            <App bookId="1" />
+          </ModalProvider>
+        </ColorProvider.Provider>
+      </StateProvider>
     </ThemeProvider>
   </ApolloProvider>,
   document.getElementById('root'),

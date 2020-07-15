d8d0ec25a63f741bb1c2bc11fe174558c952e1b9
Indimeco <18373729+Indimeco@users.noreply.github.com>
2020-07-16 08:25:25 +1000

    chore(app): remove unused card

--- packages/app/src/components/Card/Card.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import { Container } from './Card.style';
-
-export const Card: React.FunctionComponent = ({ children }) => <Container>{children}</Container>;
-
-export default Card;
